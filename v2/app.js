e3d87bea82f0ade34692059ce5ef2697c5c001b0
JavierPicon89 <javierppicon@gmail.com>
2026-07-05 01:16:47 -0500

    Create MapLibre prototype

---
+++ v2/app.js
@@ -0,0 +1,11 @@
+const map = new maplibregl.Map({
+    container: "map",
+
+    style: "https://demotiles.maplibre.org/style.json",
+
+    center: [15, 48],
+
+    zoom: 3
+});
+
+map.addControl(new maplibregl.NavigationControl(), "top-left");
